adca933d7a02bc4dc582ea69d72bedde1d7114f7
kevin.valsjo <kevin.valsjo@elev.ga.ntig.se>
2023-10-25 11:05:36 +0200

    progress

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Anvandare myuser = new Anvandare("hej", "bok@email.se");
+        Anvandare myuser2 = new Anvandare("bok2", "bok2@email.se");
+
+        System.out.println(myuser.namn);
+        System.out.println(myuser2.namn);
+
+
+        Scanner tangentbord = new Scanner(System.in);
+        myuser.namn = tangentbord.next();
+        System.out.println(myuser.namn);
+        String test = "y";
+
+     /* while (test.equalsIgnoreCase("Y")) {
+            //myuser.count();
+           test = tangentbord.nextLine();
+
+        }
+        System.out.println(myuser.getCounter());
+*/
+
+    }
+}
